b6ba082b8cf3ebb74ca7621ce5fba590b78248e7
drasti492 <modes7267@gmail.com>
2026-04-29 01:40:47 +0300

    your commit message

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,1 +1,9 @@
-c
+const router = require("express").Router();
+const ctrl = require("../controllers/paymentController");
+const auth = require("../middleware/auth");
+
+router.post("/stk", auth, ctrl.stkPush);
+router.get("/status/:reference", auth, ctrl.checkStatus);
+router.post("/callback", ctrl.payheroCallback);
+
+module.exports = router;
